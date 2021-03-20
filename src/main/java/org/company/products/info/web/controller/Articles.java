4e4c5b4d616b7c78564c3a221c9e349bf24317a4
konianzero <konianzero@gmail.com>
2021-03-20 21:04:16 +0300

    Add Logging: - logback.xml; - logging information in controllers;

--- src/main/java/org/company/products/info/web/controller/Articles.java
+++ src/main/java/org/company/products/info/web/controller/Articles.java
@@ -1,5 +1,8 @@
 package org.company.products.info.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,7 @@ import org.company.products.info.service.ArticleService;
 @RequestMapping(value = Articles.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class Articles {
     public static final String REST_URL = "/articles";
+    private static final Logger log = LoggerFactory.getLogger(Articles.class);
 
     private final ArticleService articleService;
 
@@ -25,6 +29,7 @@ public class Articles {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Article> createWithLocation(@RequestBody Article article) {
+        log.info("Create new article - {}", article);
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
                                                           .path(REST_URL + "/{id}")
                                                           .buildAndExpand(article.getId())
@@ -35,23 +40,27 @@ public class Articles {
 
     @GetMapping("/{id}")
     public Article get(@PathVariable int id) {
+        log.info("Get article with id={}", id);
         return articleService.get(id);
     }
 
     @GetMapping
     public List<Article> getAll() {
+        log.info("Get all articles");
         return articleService.getAll();
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@RequestBody Article article) {
+        log.info("Update article - {}", article);
         articleService.update(article);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int id) {
+        log.info("Delete article with id={}", id);
         articleService.delete(id);
     }
 }
--- src/main/java/org/company/products/info/web/controller/Products.java
+++ src/main/java/org/company/products/info/web/controller/Products.java
@@ -1,5 +1,8 @@
 package org.company.products.info.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,7 @@ import org.company.products.info.service.ProductService;
 @RequestMapping(value = Products.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class Products {
     public static final String REST_URL = "/products";
+    private static final Logger log = LoggerFactory.getLogger(Products.class);
 
     private final ProductService productService;
 
@@ -25,6 +29,7 @@ public class Products {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Product> createWithLocation(@RequestBody Product product) {
+        log.info("Create new product - {}", product);
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
                                                           .path(REST_URL + "/{id}")
                                                           .buildAndExpand(product.getId())
@@ -35,23 +40,27 @@ public class Products {
 
     @GetMapping("/{id}")
     public Product get(@PathVariable int id) {
+        log.info("Get product with id={}", id);
         return productService.get(id);
     }
 
     @GetMapping
     public List<Product> getAll() {
+        log.info("Get all products");
         return productService.getAll();
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@RequestBody Product product) {
+        log.info("Update product - {}", product);
         productService.update(product);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int id) {
+        log.info("Delete product with id={}", id);
         productService.delete(id);
     }
 }
