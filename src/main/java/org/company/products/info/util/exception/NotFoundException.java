3fee1f3ce02a32f5d4c51f4503f563a86aaa22f9
konianzero <konianzero@gmail.com>
2021-03-21 23:55:49 +0300

    Add exceptions: - IllegalRequestDataException.java - NotFoundException.java

---
+++ src/main/java/org/company/products/info/util/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package org.company.products.info.util.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
