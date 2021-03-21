c6fd37ed2d3af78f6837e7e98475d46c8e9b3ffa
konianzero <konianzero@gmail.com>
2021-03-21 23:54:59 +0300

    Add custom delete method with int return type to check result of operation.

--- src/main/java/org/company/products/info/repository/ArticleRepository.java
+++ src/main/java/org/company/products/info/repository/ArticleRepository.java
@@ -2,10 +2,18 @@ package org.company.products.info.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import org.company.products.info.model.Article;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer>, JpaSpecificationExecutor<Article> {
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Article a WHERE a.id = ?1")
+    int delete(int id);
 }
