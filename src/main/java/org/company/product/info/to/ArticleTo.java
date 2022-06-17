9d8efefae094d290faf40edfd94764ed447fbc2d
konianzero <konianzero@gmail.com>
2022-06-17 17:29:37 +0300

    refactor: Refactor ArticleTo.java

--- src/main/java/org/company/product/info/to/ArticleTo.java
+++ src/main/java/org/company/product/info/to/ArticleTo.java
@@ -4,10 +4,14 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.company.product.info.HasId;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class ArticleTo implements HasId {
@@ -23,58 +27,4 @@ public class ArticleTo implements HasId {
     @NotBlank
     @Size(min = 30, max = 5000)
     private String content;
-
-    public ArticleTo() {
-    }
-
-    public ArticleTo(Integer id, String name, int productId, String content) {
-        this.id = id;
-        this.name = name;
-        this.productId = productId;
-        this.content = content;
-    }
-
-    @Override
-    public Integer getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getProductId() {
-        return productId;
-    }
-
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleTo{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", productId=" + productId +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
