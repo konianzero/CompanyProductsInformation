be5566dec707c622ba65267b43a41696470171aa
konianzero <konianzero@gmail.com>
2022-06-29 18:16:54 +0300

    fix(persistence.repository): Fix IllegalArgumentException 'Not a managed type: class model.Product', add @EntityScan

--- persistence/repository/src/main/java/repository/PersistenceApplication.java
+++ persistence/repository/src/main/java/repository/PersistenceApplication.java
@@ -2,8 +2,10 @@ package repository;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("model")
 public class PersistenceApplication {
 // TODO
 //  - https://www.baeldung.com/spring-data-derived-queries
