0365e7ea58a8568817a00523f28c5076ec502394
konianzero <konianzero@gmail.com>
2022-07-15 20:13:12 +0300

    fix(front): CleanUp after merge

--- front/src/main/java/org/company/front/config/AppConfig.java
+++ front/src/main/java/org/company/front/config/AppConfig.java
@@ -1,8 +1,5 @@
 package org.company.front.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.company.front.util.JsonUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +7,6 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
-
-    @Autowired
-    public void storeObjectMapper(ObjectMapper objectMapper) {
-        JsonUtil.setMapper(objectMapper);
-    }
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
