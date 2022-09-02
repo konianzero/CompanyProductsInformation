2c617a8603d438c023289c5481abe6734cd7aacf
konianzero <konianzero@gmail.com>
2022-09-02 21:12:11 +0300

    fix(front): Fix ProductsTest.java

--- front/controller/src/test/java/org/company/front/web/controller/ProductsTest.java
+++ front/controller/src/test/java/org/company/front/web/controller/ProductsTest.java
@@ -1,12 +1,9 @@
 package org.company.front.web.controller;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.company.cache.external.jms.to.ProductsInfoRequest;
 import org.company.cache.service.HazelcastCache;
 import org.company.front.TestData;
 import org.company.front.service.ProductService;
-import org.company.cache.external.jms.JmsClient;
 import org.company.cache.external.jms.to.ProductInfo;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -24,6 +21,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.company.front.TestData.PRODUCTS_VIEW;
 import static org.company.front.TestData.PRODUCT_2;
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
@@ -35,13 +33,12 @@ class ProductsTest {
 
     @Autowired(required=true)
     private ProductService client;
-    @MockBean
-    private HazelcastCache hazelcastCache;
-    ProductsInfoRequest productInfoRequestMock = Mockito.mock(ProductsInfoRequest.class);
     @Autowired
     private MockRestServiceServer server;
     @Autowired
     private ObjectMapper objectMapper;
+    @MockBean
+    private HazelcastCache hazelcastCache;
 
     @Test
     void create() throws Exception {
@@ -64,41 +61,43 @@ class ProductsTest {
                 .isEqualTo(TestData.PRODUCT_NEW);
     }
 
-    // TODO - Fix test
     @Test
     void get() throws Exception {
         // given
         String productOne = objectMapper.writeValueAsString(TestData.PRODUCT_1_VIEW);
         // when
-        Mockito.when(hazelcastCache.getData(productInfoRequestMock)).thenReturn(List.of(new ProductInfo()));
+        Mockito.when(hazelcastCache.getData(any())).thenReturn(List.of(new ProductInfo()));
         this.server.expect(requestTo(ProductService.URI_PRODUCTS + "/100000"))
                 .andExpect(method(HttpMethod.GET))
                 .andRespond(withSuccess(productOne, MediaType.APPLICATION_JSON));
         // then
         assertThat(this.client.get(100000))
+                .isNotNull()
                 .usingRecursiveComparison()
                 .isEqualTo(TestData.PRODUCT_1_VIEW);
     }
 
-    // TODO - Fix test
     @Test
     void getAll() throws Exception {
         // given
-        objectMapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
         String requestQuery = "filterBy=id&filter=100001";
         String products = objectMapper.writeValueAsString(PRODUCTS_VIEW);
         // when
-        Mockito.when(hazelcastCache.getData(productInfoRequestMock)).thenReturn(List.of(new ProductInfo()));
+        List<ProductInfo> list = List.of(new ProductInfo());
+        Mockito.when(hazelcastCache.getData(any())).thenReturn(list);
         this.server.expect(requestTo(ProductService.URI_PRODUCTS + "?" + requestQuery))
+                .andExpect(method(HttpMethod.GET))
                 .andRespond(withSuccess(products, MediaType.APPLICATION_JSON));
         // then
         assertThat(this.client.getAll(requestQuery))
+                .isNotNull()
                 .isNotEmpty()
                 .filteredOn(productView ->
                         productView.getId().equals(PRODUCT_2.getId())
                         && productView.getName().equals(PRODUCT_2.getName())
                         && productView.getDescription().equals(PRODUCT_2.getDescription())
                         && productView.getImplementationCost() == PRODUCT_2.getImplementationCost()
-                ).hasSize(1);
+                )
+                .hasSize(1);
     }
 }
